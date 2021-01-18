2c6f53817f07f49ac233515eb5d6554b52405cf9
Yevhenii Babenko <yevheniibabenko@gmail.com>
2021-01-18 23:51:00 +0200

    added js files of slider slick

---
+++ hw-9/js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function (){
+    $('.slider').slick({
+        // centerMode:true,
+        // centerPadding: '18px',
+        arrows:true,
+        dots:true,
+        slidesToShow:4,
+        autoplay:true,
+        autoplaySpeed:2500,
+        variableWidth:true,
+        // mobileFirst:true,
+        // responsive:[
+        //     {
+        //         breakpoint: 720,
+        //         settings:{
+        //             slidesToShow:1
+        //         }
+        //     }
+        // ]
+    });
+})
